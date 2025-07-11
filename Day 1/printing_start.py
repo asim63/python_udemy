97e1b82dce3eace97dfaea27202e2905d856a844
asim63 <asimdkt63@gmail.com>
2025-07-11 19:39:47 +0545

    completed day 1 of Udemy course

---
+++ Day 1/printing_start.py
@@ -0,0 +1,17 @@
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print(\"what to print\")\n")
+
+#string manipulation
+print(" hello\n world")
+
+#concatenation
+print("hello"+" I am Asim")
+
+#indentation error
+# print("hello:")
+
+#debugging 
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print("e.g. print('Hello' + ' ' + 'World')")
