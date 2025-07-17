96f889837bd93992e51788055ac8d2fb6d2ec1f0
asim63 <asimdkt63@gmail.com>
2025-07-17 19:40:18 +0545

    Day 7 completed.

--- Day 7/Challenge5.py
+++ Day 7/Challenge5.py
@@ -1,10 +1,10 @@
 import random
 from hangman_arts import logo, stages 
 from hangman_words import word_list
-import os
+
 chosen_word = random.choice(word_list)
 print(logo)
-# print(f"Psssts. The word is {chosen_word}")
+print(f"Psssts. The word is {chosen_word}")
 result = True
 display = []
 lives = 6
@@ -13,34 +13,32 @@ for i in chosen_word:
     display.append('_')
 
 while(result):
+
     guess_letter = input("Guess a letter: ").lower()
-    cls
-    count = 0
+    count = 0 
     if guess_letter in display:
-        print(f"{guess_letter}?. You have already used this word.")
-    for i in range(0,len(chosen_word)):
-        if chosen_word[i] == guess_letter:
-            display[i] = guess_letter
+        print(f"{guess_letter}?. You have already guessed this word.")
+        print(f"{' '.join(display)}")
+    else:    
+        for i in range(0,len(chosen_word)):
+            if chosen_word[i] == guess_letter:
+                    display[i] = guess_letter
+            else:
+                    count += 1 
         else:
-            count += 1 
+            if count == len(chosen_word):
+                print(f"{guess_letter} is not in the word. ")
+                lives -= 1
+        
+            print(f"{' '.join(display)}")
 
-    if count == len(chosen_word):
-        print(f"{guess_letter} is not in the word. ")
-        lives -= 1
-   
-    print(f"{' '.join(display)}")
+        
+        if lives == 0:
+            result = False
+            print(f"You lose. The word was {chosen_word}")
 
-    
-    if lives == 0:
-        result = False
-        print(stages[0])
-        print(f"You lose. The word was {chosen_word}")
+        if '_' not in display:
+            result = False
+            print("You Win")
 
-    elif '_' in display:
-        result = True
-        print(stages[lives])
-
-    else:
-        result = False
-        print("You Win")
-
+    print(stages[lives])
