405fb6386f50b1182b1cc399a71b2901d0125f79
asim63 <asimdkt63@gmail.com>
2025-07-24 18:09:11 +0545

    Higher_or_lower game complete

--- Day 14/Higher_or_Lower.py
+++ Day 14/Higher_or_Lower.py
@@ -3,7 +3,8 @@ import os
 from art import logo,vs
 from game_data import data
 game_is_over = False
-score = 0
+
+yes = True
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear' )
 
@@ -11,36 +12,42 @@ def calculate(guess,A,B):
     followers_A = int(A['follower_count'])
     followers_B = int(B['follower_count'])
 
-    if ((guess == 'A' or guess =='a') and followers_A >= followers_B) or ((guess == 'B' or guess =='b') and followers_B >= followers_A):
+    if (guess =='a' and followers_A >= followers_B) or (guess =='b' and followers_B >= followers_A):
         return 1
     else:
         return 2
-    
-a = random.choice(data)
-while(not game_is_over):
-    print(logo)
-    if score > 0:
-        print(f"You're right! Current Score: {score}")
+while(yes):
+    score = 0
+    print(logo)  
+    a = random.choice(data)
+    while(not game_is_over):
 
-    b = random.choice(data)
+        if score > 0:
+            print(f"You're right! Current Score: {score}")
 
-    while(b['follower_count'] == a['follower_count']):
         b = random.choice(data)
 
-    print(f"Compare A: {a['name']}, a {a['description']}, from {a['country']} ")
-    print(f"A_followers = {a['follower_count']}")
-    print(vs)
-    
-    print(f"Against B: {b['name']}, a {b['description']}, from {b['country']} ")
-    print(f"B_followers = {b['follower_count']}")
-    choose = input("Who has more followers? Type 'A' or 'B': ")
-    clear_screen()
+        while(b == a):
+            b = random.choice(data)
 
-    if calculate(choose,a,b) == 1:
-        score += 1
-        a = b 
-    else:
-        game_is_over = True
+        print(f"Compare A: {a['name']}, a {a['description']}, from {a['country']} ")
+        # print(f"A_followers = {a['follower_count']}")
+        print(vs)
+        
+        print(f"Against B: {b['name']}, a {b['description']}, from {b['country']} ")
+        # print(f"B_followers = {b['follower_count']}")
+        choose = input("Who has more followers? Type 'A' or 'B': ").lower()
+        clear_screen()
         print(logo)
-        print(f"Sorry, that's wrong. Final Score : {score}.")
-    
+        if calculate(choose,a,b) == 1:
+            score += 1
+            a = b 
+        else:
+            game_is_over = True
+            print(f"Sorry, that's wrong. Final Score : {score}.")
+
+    game_is_over = False
+    again = input("Do you want to play the game once again? Type 'y' or 'n': ").lower()
+    if not again == 'y':
+        yes = False
+    clear_screen()
