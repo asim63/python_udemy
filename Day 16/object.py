4ff015540cc8e42bf6719fc8ef37e2c8ebb09b84
asim63 <asimdkt63@gmail.com>
2025-07-26 14:06:25 +0545

    Learn turtle package

--- Day 16/object.py
+++ Day 16/object.py
@@ -5,5 +5,12 @@ print(a)
 
 a.speed = 30
 
+a.shape('turtle')
+a.color('cyan3')
+a.forward(100)
 my_screen = Screen()
-print(my_screen.canvheight)
+
+# print(my_screen.canvheight)
+# print(my_screen.canvwidth)
+my_screen.exitonclick()
+  
