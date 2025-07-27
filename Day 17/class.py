9a163be8c349ad4b654ed2b6bac5d021bbb01758
asim63 <asimdkt63@gmail.com>
2025-07-27 09:41:01 +0545

    learn create custom classes

--- Day 17/class.py
+++ Day 17/class.py
@@ -3,20 +3,29 @@ class User:
         print("new user being created....")
         self.name = username
         self.id = user_id
+        self.followers = 0
+        self.following = 0
     
-    
+    def follow(self, user):
+        user.followers += 1
+        self.following +=1
 # user_1 = User()
 
 # user_1.id = "001"
 # user_1.username = "asim"
 
-# print(int(user_1.id))
-
-# user_2 = User()
-# user_2.name = "jpt"
-
-# print(user_2.name)
+# print(user_1.id)
 
 user_3 = User('Asim',78)
 print(user_3.name)
 print(user_3.id)
+
+user_1 = User('Bigay',56)
+
+user_1.follow(user_3)
+
+print(user_1.followers)
+print(user_1.following)
+print(user_3.followers)
+print(user_3.following)
+
