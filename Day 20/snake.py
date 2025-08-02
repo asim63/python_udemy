fa9a64073327e0cd72cdc8e994a5ebda0643cc28
asim63 <asimdkt63@gmail.com>
2025-08-02 13:35:28 +0545

    Add event listener

--- Day 20/snake.py
+++ Day 20/snake.py
@@ -1,21 +1,39 @@
 from turtle import Turtle , Screen
+MOVE_DISTANCE = 20
 class Snake:
 
     def __init__(self):
         self.segment = []
+        self.create_snake()
+    
+    def create_snake(self):
         x = 0
         for i in range(3):
-            t = Turtle()
-            t.shape('square')
-            t.penup()
-            t.color('white')
-            t.setpos(x,0)
-            self.segment.append(t)
+            self.t = Turtle()
+            self.t.shape('square')
+            self.t.penup()
+            self.t.color('white')
+            self.t.setpos(x,0)
+            self.segment.append(self.t)
             x += -20
 
+
+    def up(self):
+        self.segment[0].setheading(90)
+    
+    def left(self):
+        self.segment[0].setheading(180)
+    
+    def down(self):
+        self.segment[0].setheading(270)
+    
+    def right(self):
+        self.segment[0].setheading(0)
+    
+        
     def move(self):
         for i in range(len(self.segment)-1 ,0 ,-1):
             new_x = self.segment[i-1].xcor()
             new_y = self.segment[i-1].ycor()
             self.segment[i].setpos(new_x, new_y)
-        self.segment[0].fd(20)
+        self.segment[0].fd(MOVE_DISTANCE)
--- Day 20/snake_game.py
+++ Day 20/snake_game.py
@@ -8,20 +8,23 @@ screen.setup(width = 600, height =600)
 screen.bgcolor('black')
 screen.title('Snake Game')
 screen.tracer(0)
-screen.listen()
-x = 0 
-y = 0
+
+
 game_is_on = True
-segment = []
-
-
 snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.right,"Right")
 
 
 while(game_is_on):
     screen.update()
     time.sleep(0.1)
     snake.move()
+
 
 screen.exitonclick()
 
