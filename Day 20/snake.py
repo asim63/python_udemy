3d59621f29ec37619c33c5ac05ae1e16f1262b0b
asim63 <asimdkt63@gmail.com>
2025-08-02 13:46:07 +0545

    gave direction

--- Day 20/snake.py
+++ Day 20/snake.py
@@ -1,10 +1,15 @@
 from turtle import Turtle , Screen
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 class Snake:
 
     def __init__(self):
         self.segment = []
         self.create_snake()
+        self.head = self.segment[0]
     
     def create_snake(self):
         x = 0
@@ -19,16 +24,20 @@ class Snake:
 
 
     def up(self):
-        self.segment[0].setheading(90)
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
     
     def left(self):
-        self.segment[0].setheading(180)
+        if self.head.heading() != RIGHT:    
+            self.head.setheading(LEFT)
     
     def down(self):
-        self.segment[0].setheading(270)
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
     
     def right(self):
-        self.segment[0].setheading(0)
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
     
         
     def move(self):
