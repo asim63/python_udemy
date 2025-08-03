2059a4204c4e312c821d95df47ed1b063d4082c4
asim63 <asimdkt63@gmail.com>
2025-08-03 08:11:32 +0545

    Food create and collide

--- Day 20/snake_game.py
+++ Day 20/snake_game.py
@@ -1,7 +1,7 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 import time
 from snake import Snake
-
+from food import Food
 #Screen setup
 screen = Screen()
 screen.setup(width = 600, height =600)
@@ -12,19 +12,23 @@ screen.tracer(0)
 
 game_is_on = True
 snake = Snake()
+food = Food()
+
 
 screen.listen()
 screen.onkey(snake.up,"Up")
 screen.onkey(snake.down,"Down")
-screen.onkey(snake.left,"Left")
 screen.onkey(snake.right,"Right")
-
+screen.onkey(snake.left,"Left")
 
 while(game_is_on):
     screen.update()
     time.sleep(0.1)
     snake.move()
-
+    
+    #detect collison with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 screen.exitonclick()
 
