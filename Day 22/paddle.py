97d8d0d319fe6bcf66de8dc058d6cea00cdfcaf2
asim63 <asimdkt63@gmail.com>
2025-08-04 22:11:13 +0545

    use shapesize function for paddle instead of segments

--- Day 22/paddle.py
+++ Day 22/paddle.py
@@ -4,37 +4,17 @@ class Paddle(Turtle):
         super().__init__()
         self.x = x
         self.y = y
-        self.segment = []
-        self.hideturtle()
+        self.shape("square")
+        self.shapesize(stretch_wid  =  5, stretch_len = 1)
         self.color('white')
-        self.create_paddle()
-        self.mid = self.segment[1]
-        self.head = self.segment[0]
-        self.tail = self.segment[2]
-        
-    def create_paddle(self):
-        for i in range(4):
-            self.make_paddle(self.x,self.y)
-            self.y -=20
-        
-    def make_paddle(self,x,y):
-        self.t = Turtle()
-        self.t.shape('square')
-        self.t.setheading(90)
-        self.t.penup()
-        self.t.color('white')
-        self.t.setpos(x,y)
-        self.segment.append(self.t)
-        
+        self.setpos(self.x,self.y)  
+
     def paddle_up(self):
-        if self.mid.ycor() < 310:
-            self.mid.fd(20)
+        if self.ycor() < 310:
+            new_y = self.ycor() + 20
+            self.setpos(self.x, new_y)
         
     def paddle_down(self):
-        if self.mid.ycor() > -310:
-            self.mid.bk(20)
-        
-    def paddle_move(self):
-            new_y = self.mid.ycor()
-            self.head.setpos(self.x,new_y + 20)
-            self.tail.setpos(self.x,new_y - 20)
+        if self.ycor() > -310:
+            new_y = self.ycor() - 20
+            self.setpos(self.x, new_y)
--- Day 22/pong_game.py
+++ Day 22/pong_game.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
 import time
+from ball import Ball
 screen = Screen()
 screen.bgcolor('black')
 screen.setup(width = 900, height = 700)
@@ -11,6 +12,7 @@ game_is_on = True
 paddle1 = Paddle( x= -438, y = 0)
 paddle2 = Paddle( x = 430, y = 0)
 
+ball = Ball()
 screen.listen()
 screen.onkey(paddle1.paddle_up, "w")
 screen.onkey(paddle1.paddle_down, "s")
@@ -20,9 +22,7 @@ screen.onkey(paddle2.paddle_down, "Down")
 
 while(game_is_on):
     screen.update()
-    # time.sleep(0.1)    
-    paddle1.paddle_move()
-    paddle2.paddle_move()
+    time.sleep(0.1)    
 
 
 screen.exitonclick()
