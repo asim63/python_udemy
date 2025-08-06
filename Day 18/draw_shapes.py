056a5f1f77fbefa0a480bf84aa9b518ceefc752b
asim63 <asimdkt63@gmail.com>
2025-08-06 18:32:59 +0545

    Day 23 start, Cross the road Game

--- Day 18/draw_shapes.py
+++ Day 18/draw_shapes.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
 import random 
 t = Turtle()
+t.speed(0)
 def change_colour():
     R = random.random()
     B = random.random()
@@ -8,14 +9,18 @@ def change_colour():
     
     t.color(R,G,B)
     
-for item in range(3,11):
+for item in range(25,50):
     
     n = 360/item
     change_colour()
     for _ in range (item):
         
-        t.fd(100)
+        t.fd(20)
         t.right(n)
+        
+    new_x = t.xcor()
+    y = t.ycor()
+    t.setpos(new_x ,y + 2)
     
     
 screen = Screen()
