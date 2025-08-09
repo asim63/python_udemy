cfdef33f585519676a012ceb7aac024104fd089d
asim63 <asimdkt63@gmail.com>
2025-08-09 10:27:58 +0545

    remove 'r'

--- Day 24/Mail Merge Project Start/mail_merge.py
+++ Day 24/Mail Merge Project Start/mail_merge.py
@@ -1,15 +1,6 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
-        
 
-with open(r"Day 24\Mail Merge Project Start\Input\Letters\starting_letter.txt", mode = 'r') as file:
-    with open(r"Day 24\Mail Merge Project Start\Input\Names\invited_names.txt",mode = 'r') as name_file:
+with open(r"Day 24\Mail Merge Project Start\Input\Letters\starting_letter.txt") as file:
+    with open(r"Day 24\Mail Merge Project Start\Input\Names\invited_names.txt") as name_file:
         name_list = name_file.readlines()
         text = file.read()
         # print(text)
