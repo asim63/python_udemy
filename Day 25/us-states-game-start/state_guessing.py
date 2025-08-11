52cd3b69e42a7b79328335f2836603e80c5339f4
asim63 <asimdkt63@gmail.com>
2025-08-11 18:28:43 +0545

    add csv file of unguessed state

--- Day 25/us-states-game-start/state_guessing.py
+++ Day 25/us-states-game-start/state_guessing.py
@@ -10,13 +10,12 @@ data = pd.read_csv(r"Day 25\us-states-game-start\50_states.csv")
 screen.addshape(image)
 turtle.shape(image)
 
-game = True
 correct_guess = []
-while (len(correct_guess) < 50 and game):
+while (len(correct_guess) < 50):
     answer_state = screen.textinput(title = f"{len(correct_guess)}/50 States Correct", prompt = "Input state's name").title()
     if answer_state in data.state.values and not answer_state in correct_guess :
         new_data = data[data.state == answer_state]
-        print(new_data)
+        # print(new_data)
         x = int(new_data.x.iloc[0])
         y = int(new_data.y.iloc[0])
         t = turtle.Turtle()
@@ -25,7 +24,20 @@ while (len(correct_guess) < 50 and game):
         t.teleport(x,y)
         t.write(f"{answer_state}")
         correct_guess.append(answer_state)  
-    elif answer_state == "Exit":
-        game = False
-turtle.mainloop()
+    if answer_state == "Exit":
+        break
+print(correct_guess)
+unguessed_state = []
+unguessed_x = []
+unguessed_y = []
+for state in data.state.values:
+    if state not in correct_guess:
+        unguessed_state.append(state)
+# unguessed_data = {
+#     "state" : unguessed_state,
 
+# }
+
+# print(unguessed_state)
+states_to_learn = pd.DataFrame(unguessed_state)
+states_to_learn.to_csv("Day 25/us-states-game-start/states_to_learn.csv")
