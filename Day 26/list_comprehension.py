1a5c0561fed1a3a3ee7719eb5876b0d79fd26cdf
asim63 <asimdkt63@gmail.com>
2025-08-12 08:54:55 +0545

    square the list using list comprehension

--- Day 26/list_comprehension.py
+++ Day 26/list_comprehension.py
@@ -9,3 +9,11 @@ print(letter_list)
 
 number_list = [n*2 for n in range(1,5)]
 print(number_list)
+
+number_list = [n for n in range(1,10) if n % 3 ==0]
+print(number_list)
+
+names = ['Alex','Beth', 'Caroline', 'Dave', 'Eleanor', 'Freddie']
+
+new_name = [name.upper() for name in names if len(name) > 5]
+print(new_name)
