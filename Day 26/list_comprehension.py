12e6b1977e8d366b0d76bfd01d84241bcaf4e3a5
asim63 <asimdkt63@gmail.com>
2025-08-12 08:46:34 +0545

    Day 26 start, learn list comprehension

---
+++ Day 26/list_comprehension.py
@@ -0,0 +1,11 @@
+numbers = [1, 2, 3]
+
+new_list = [n+1 for n in numbers]
+print(new_list)
+
+name = "Asim"
+letter_list = [letter for letter in name]
+print(letter_list)
+
+number_list = [n*2 for n in range(1,5)]
+print(number_list)
