61d83ca49300955efe779632a12319042f1611e5
asim63 <asimdkt63@gmail.com>
2025-08-15 21:52:26 +0545

    Day 27 resume

--- Day 27/tkinter_module.py
+++ Day 27/tkinter_module.py
@@ -9,6 +9,12 @@ window.minsize(width = 500, height = 400)
 my_label = tkinter.Label(text = "I am Asim", font = ("Arial", 24, "bold"))
 my_label.pack(side = 'left')
 
+#you can change text like this as well
+my_label['text'] = 'new text'
+my_label.config(text = "New Text")
+
+
+
 
 window.mainloop()
     
