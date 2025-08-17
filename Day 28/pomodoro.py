1f054722e9beca35a20f4debfdf110600b35596f
asim63 <asimdkt63@gmail.com>
2025-08-17 21:22:36 +0545

    Countdown

--- Day 28/pomodoro.py
+++ Day 28/pomodoro.py
@@ -12,21 +12,46 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    countdown(5)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def countdown(count):
+    canvas.itemconfig(timer_text, text = str(count))
+    if count > 0 :
+       window.after(1000, countdown, count - 1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
 window.config(padx = 100, pady = 50, bg = YELLOW)
+
+
+#TIMER
+label = Label(text = "Timer", font =("Courier", 40, "bold "), bg = YELLOW, fg = GREEN )
+label.grid(row = 0, column = 1)
+
+
+#image
 canvas = Canvas(width = 200, height = 224, bg = YELLOW, highlightthickness = 0)
 tomato_img = PhotoImage(file = r"Day 28\tomato.png")
 canvas.create_image(100,112, image = tomato_img)
-canvas.create_text(100,130, text = " 00:00 ", font = (FONT_NAME, 35, "bold"), fill = 'white')
-canvas.pack()
+timer_text = canvas.create_text(100,130, text = "5", font = (FONT_NAME, 35, "bold"), fill = 'white')
+canvas.grid(row = 1,column = 1)
 
+#button1
+button1 = Button(text = "Start", command = start_timer, highlightthickness = 0)
+button1.grid(row = 2,column = 0)
+button1.config(padx = 5, pady = 2)
 
+#button1
+button2 = Button(text = "Reset", highlightthickness = 0)
+button2.grid(row = 2,column = 2)
+button2.config(padx = 5, pady = 2)
 
+#checkmark
+label2 = Label(text = "✔", font = (FONT_NAME, 14),  bg = YELLOW, fg = GREEN)
+label2.grid(row = 3,column = 1)
 
 
 
