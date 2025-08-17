67e9be75766aba80089fa950d300eb0d0aa4bd93
asim63 <asimdkt63@gmail.com>
2025-08-17 21:47:38 +0545

    learn dynamic typing

--- Day 28/pomodoro.py
+++ Day 28/pomodoro.py
@@ -13,11 +13,20 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    countdown(5)
+    countdown(14)
 
+def reset_timer():
+    canvas.itemconfig(timer_text, text = "00:00")
+    window.after(1000, countdown, 0)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def countdown(count):
-    canvas.itemconfig(timer_text, text = str(count))
+    seconds = count % 60
+    minutes = count // 60
+    if seconds < 10:
+        seconds = f'0{seconds}'
+        
+    
+    canvas.itemconfig(timer_text, text = f"{minutes}:{seconds}")
     if count > 0 :
        window.after(1000, countdown, count - 1)
 
@@ -36,7 +45,7 @@ label.grid(row = 0, column = 1)
 canvas = Canvas(width = 200, height = 224, bg = YELLOW, highlightthickness = 0)
 tomato_img = PhotoImage(file = r"Day 28\tomato.png")
 canvas.create_image(100,112, image = tomato_img)
-timer_text = canvas.create_text(100,130, text = "5", font = (FONT_NAME, 35, "bold"), fill = 'white')
+timer_text = canvas.create_text(100,130, text = "00:00", font = (FONT_NAME, 35, "bold"), fill = 'white')
 canvas.grid(row = 1,column = 1)
 
 #button1
@@ -45,7 +54,7 @@ button1.grid(row = 2,column = 0)
 button1.config(padx = 5, pady = 2)
 
 #button1
-button2 = Button(text = "Reset", highlightthickness = 0)
+button2 = Button(text = "Reset", command = reset_timer,  highlightthickness = 0)
 button2.grid(row = 2,column = 2)
 button2.config(padx = 5, pady = 2)
 
