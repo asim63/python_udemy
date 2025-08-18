1780ea28a219ecb7d9db0326f6693876fd836bb6
asim63 <asimdkt63@gmail.com>
2025-08-18 18:06:18 +0545

    Password Generator create

--- Day 29/password_generator.py
+++ Day 29/password_generator.py
@@ -1,17 +1,52 @@
 from tkinter import *
+import random
+from tkinter import messagebox
+import pyperclip
 YELLOW = "#f7f5dd"
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_pw():
-    pass
+        #Password Generator Project
+    import random
+    input3.delete(0, END)
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_list = []
+    
+    password_letters = [random.choice(letters) for i in range(nr_letters)]
+    
+    password_symbols = [random.choice(symbols) for i in range(nr_symbols)]
+
+    password_numbers = [random.choice(numbers) for i in range(nr_numbers)]
+       
+    password_list = password_letters + password_symbols + password_numbers
+    
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+    pyperclip.copy(password)
+    input3.insert(0, string = password)
+    
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
     website_name = input1.get()
     email_name = input2.get()
-    password = input3.get()
+    password_name = input3.get()
     
-    with open("Day 29/ data.txt", mode = 'a') as file:
-        file.write(f"{website_name} | {email_name} | {password}\n")
+    if website_name == "" or email_name == "" or password_name == "":
+        messagebox.showerror(title = "Error 404", message = "Please dont leave any fields empty")
+    
+    else:
+        is_ok = messagebox.askokcancel(title = "website", message = f"These are the details entered:\nEmail : {email_name}\n"
+                            f"Password : {password_name}\n\nIs it ok to save?")
+        if is_ok == True:
+            with open("Day 29/ data.txt", mode = 'a') as file:
+                file.write(f"{website_name} | {email_name} | {password_name}\n")
         input1.delete(0,END)
         input3.delete(0,END)
 
