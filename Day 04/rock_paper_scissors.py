2ee7153f598833c5f994fb2fa7b4a1bfa1d27cfe
Asim Poudel <asimdkt63@gmail.com>
2025-10-16 21:40:34 +0545

    Update rock_paper_scissors.py

--- Day 04/rock_paper_scissors.py
+++ Day 04/rock_paper_scissors.py
@@ -25,6 +25,7 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+
 hand = [rock,paper,scissors]
 user = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
 comp = random.randint(0,2)
